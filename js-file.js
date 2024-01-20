55aeaad59aadede7d2ca5a9091236ac3ab723268
Amelie Sutton <112052610+eastouest@users.noreply.github.com>
2024-01-19 16:50:43 -0800

    Create 16x16 grid of divs by setting container to flex and setting each div width to 6.25%

--- js-file.js
+++ js-file.js
@@ -1,8 +1,9 @@
 const container = document.querySelector('#container');
-let gridCount = 16;
+container.style.cssText = 'display: flex; flex-wrap: wrap; flex-direction: row';
+let gridCount = 256;
 for (let i = 0; i < gridCount; i++) {
     const div = document.createElement('div');
-    div.style.cssText = 'color: blue; background: white; flex-wrap: wrap';
+    div.style.cssText = 'color: blue; background: white; width: 6.25%;'
     div.textContent = 'Hello World!';
     container.appendChild(div);
 }
