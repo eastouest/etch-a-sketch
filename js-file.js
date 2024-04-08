7d5e5e9a5db3af2da890790684ee981326edb175
Amelie Sutton <112052610+eastouest@users.noreply.github.com>
2024-04-08 15:52:11 -0700

    Add prompt with warning message to button

--- js-file.js
+++ js-file.js
@@ -3,9 +3,14 @@ container.style.cssText = 'display: flex; flex-wrap: wrap; flex-direction: row; 
 let gridCount = 256;
 const btn = document.querySelector("#btn");
 function changeGrid() {
-	alert("onclick() called");
+	let grid = prompt("Enter a value from 1 to 100");
+    if (grid > 100 || grid < 1) {
+        alert('Please enter a value between 1 and 100');
+        prompt("Enter a value from 1 to 100");
+    }
 }
-btn.style.cssText = 'width: 100%'
+const button = document.querySelector('#button-container');
+button.style.cssText = 'width: 100%'
 for (let i = 0; i < gridCount; i++) {
     const div = document.createElement('div');
     div.style.cssText = 'color: white; background: purple; width: 6.25%;'
