bb83dd3633bf15a632f386ba01122f590719d9d5
Amelie Sutton <112052610+eastouest@users.noreply.github.com>
2024-04-08 15:33:17 -0700

    Create new button with function call on click

--- js-file.js
+++ js-file.js
@@ -1,6 +1,11 @@
 const container = document.querySelector('#container');
 container.style.cssText = 'display: flex; flex-wrap: wrap; flex-direction: row; height: 100vh; width: 100%;';
 let gridCount = 256;
+const btn = document.querySelector("#btn");
+function changeGrid() {
+	alert("onclick() called");
+}
+btn.style.cssText = 'width: 100%'
 for (let i = 0; i < gridCount; i++) {
     const div = document.createElement('div');
     div.style.cssText = 'color: white; background: purple; width: 6.25%;'
